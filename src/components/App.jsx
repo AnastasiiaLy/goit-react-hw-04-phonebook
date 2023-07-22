f965d0c6abdbb5851d2008b48ce792570bbc67a3
Anastasiia Lytus <litusnastja@gmail.com>
2023-07-22 13:53:04 +0300

    phonebook

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,26 +6,19 @@ import SearchBar from './SearchBar/SearchBar';
 import css from 'App.module.css';
 
 const App = () => {
-  // const [contacts, setContacts] = useState(
-  //   JSON.parse(localStorage.getItem('contacts')) || []
-  // );
-
-  const InitialContacts = [
+  const initialContacts = [
     { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
     { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
     { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
     { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
   ];
-  const [contacts, setContacts] = useState(InitialContacts);
-  const [filter, setFilter] = useState('');
 
-  useEffect(() => {
+  const [contacts, setContacts] = useState(() => {
     const savedContacts = localStorage.getItem('contacts');
+    return savedContacts ? JSON.parse(savedContacts) : initialContacts;
+  });
 
-    if (savedContacts) {
-      setContacts(JSON.parse(savedContacts));
-    }
-  }, []);
+  const [filter, setFilter] = useState('');
 
   useEffect(() => {
     console.log('Запиши в локал стредж');
